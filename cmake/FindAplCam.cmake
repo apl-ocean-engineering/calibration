57ae87448e03c31c39ab717725a02a221eaaf514
Aaron Marburg <amarburg@apl.washington.edu>
2016-01-06 11:26:45 -0800

    Still trying to get CMake to work the way I want it to...

--- cmake/FindAplCam.cmake
+++ cmake/FindAplCam.cmake
@@ -8,18 +8,22 @@ include( ExternalProject )
 if (AplCam_SOURCE_DIR)
 	message( STATUS "Using aplcam from ${AplCam_SOURCE_DIR}" )
 
+	list(APPEND ARGS "-DCMAKE_INSTALL_PREFIX:FILEPATH=${INSTALL_DIR}")
+	list(APPEND ARGS "-DApriltags_SOURCE_DIR:FILEPATH=${Apriltags_SOURCE_DIR}")
+
 	ExternalProject_Add( aplcam
 	  DOWNLOAD_COMMAND ""
 	  SOURCE_DIR ${AplCam_SOURCE_DIR}
 	  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/aplcam
-	  CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
+	  CMAKE_ARGS ${ARGS}
 	  BUILD_COMMAND ${CMAKE_COMMAND} --build . -- aplcam
 	  INSTALL_COMMAND ""
+		DEPENDS apriltags
 	)
 
-	## If you've pulled from source, always try to rebuild the source
+	## If you've pulled from source, always try to reconfigure and rebuild the source
 	ExternalProject_Add_Step( aplcam forceconfigure
-	     COMMAND ${CMAKE_COMMAND} -E echo "Force build of aplcam "
+	     COMMAND ${CMAKE_COMMAND} -E echo "Force reconfigure and build of aplcam "
 	     DEPENDEES configure
 	     DEPENDERS build
 	     ALWAYS 1)
@@ -29,7 +33,7 @@ else ()
 	message( FATAL_ERROR "Except that's not set up yet....")
 endif ()
 
-ExternalProject_Get_Property( aplcam SOURCE_DIR )
+ExternalProject_Get_Property( aplcam SOURCE_DIR CMAKE_ARGS )
 set( aplcam_SOURCE_DIR ${SOURCE_DIR} )
 
 include( ${aplcam_SOURCE_DIR}/cmake/AplCamConfig.cmake )
---
+++ cmake/FindApriltags.cmake
@@ -0,0 +1,42 @@
+
+include( ExternalProject )
+
+if( Apriltags_SOURCE_DIR )
+  message( "Using Apriltags from directory at ${Apriltags_SOURCE_DIR}" )
+  ExternalProject_Add( apriltags
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${Apriltags_SOURCE_DIR}
+    PREFIX ${PROJECT_BINARY_DIR}/apriltags
+    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . -- apriltags
+    INSTALL_COMMAND ""
+  )
+else()
+  message( "Using Apriltags from git repo" )
+  message( "To use local version set CMake variable Apriltags_SOURCE_DIR")
+  ExternalProject_Add( apriltags
+    GIT_REPOSITORY "https://github.com/amarburg/apriltags.git"
+    PREFIX ${PROJECT_BINARY_DIR}/apriltags
+    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
+    BUILD_COMMAND ${CMAKE_COMMAND} --build .
+    INSTALL_COMMAND ""
+  )
+endif()
+
+ExternalProject_Add_Step( apriltags forceconfigure
+     COMMAND ${CMAKE_COMMAND} -E echo "Force build of apriltags"
+     DEPENDEES configure
+     DEPENDERS build
+     ALWAYS 1)
+
+ExternalProject_Get_Property( apriltags SOURCE_DIR BINARY_DIR )
+
+set( apriltags_INCLUDE_DIRS ${SOURCE_DIR}/include )
+
+message( "Apriltags source is at ${SOURCE_DIR}" )
+add_library( libapriltags STATIC IMPORTED )
+set_property( TARGET libapriltags PROPERTY IMPORTED_LOCATION ${BINARY_DIR}/libapriltags.a )
+set( apriltags_LIBS libapriltags )
+set( Apriltags_SOURCE_DIR ${SOURCE_DIR} )
+
+find_package_handle_standard_args( apriltags DEFAULT_MSG apriltags_INCLUDE_DIRS apriltags_LIBS  )
